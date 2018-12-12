da1ac5756ac334b9f15cd1de2b38f499cda3f0e5
Hiroaki Tagawa <h.tagawa@momoirospanner.app>
2018-12-12 14:19:50 +0900

    HTML

--- src/MyVendor.Weekday/src/Resource/Page/Index.php
+++ src/MyVendor.Weekday/src/Resource/Page/Index.php
@@ -1,16 +1,21 @@
 <?php
 namespace MyVendor\Weekday\Resource\Page;
 
+use BEAR\Resource\Annotation\Embed;
 use BEAR\Resource\ResourceObject;
 
 class Index extends ResourceObject
 {
-    public function onGet(string $name = 'BEAR.Sunday') : ResourceObject
+    /**
+     * @Embed(rel="weekday", src="app://self/weekday{?year,month,day}")
+     */
+    public function onGet(int $year, int $month, int $day) : ResourceObject
     {
-        $this->body = [
-            'greeting' => 'Hello ' . $name
+        $this->body += [
+            'yaer' => $year,
+            'month' => $month,
+            'day' => $day
         ];
-
         return $this;
     }
 }
