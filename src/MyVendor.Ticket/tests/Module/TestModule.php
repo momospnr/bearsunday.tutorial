849b782aae0cb0b71aff063abc9c529232f4ab53
Hiroaki Tagawa <h.tagawa@momoirospanner.app>
2018-12-12 18:48:31 +0900

    テスト作成

---
+++ src/MyVendor.Ticket/tests/Module/TestModule.php
@@ -0,0 +1,24 @@
+<?php
+namespace MyVendoer\Ticket\Module;
+
+use BEAR\Package\AbstractAppModule;
+use Ray\AuraSqlModule\AuraSqlModule;
+
+class TestModule extends AbstractAppModule
+{
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure()
+  {
+    $this->install(
+      new AuraSqlModule(
+        getenv('TKT_DB_DSN') . '_test',
+        getenv('TKT_DB_USER'),
+        getenv('TKT_DB_PASS'),
+        getenv('TKT_DB_SLAVE') 
+
+      )
+    );
+  }
+}
